0126e335b725b3e8df1c00ca98020d928cc0af40
Leviathan777-git <Gabre_346@hotmail.com>
2021-03-31 01:24:41 +0000

    (30/03) Exercícios 05 e 06

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -66,11 +66,21 @@ function Board() {
         )
     }
 
+    function RenderSquare({ i }) {
+        return (
+            <button className="square" onClick={() => selectSquare(i)}>
+                {squares[i]}
+            </button>
+        )
+    }
+
     return (
         <div>
             {/* 🐨 put the status in the div below */}
             <div className="status">{status}</div>
             <div className="board-row">
+               { //<RenderSquare i={0} />
+               }
                 {renderSquare(0)}
                 {renderSquare(1)}
                 {renderSquare(2)}
@@ -88,6 +98,7 @@ function Board() {
             <button className="restart" onClick={restart}>
                 restart
       </button>
+            <p>{JSON.stringify(squares)}</p>
         </div>
     )
 }
